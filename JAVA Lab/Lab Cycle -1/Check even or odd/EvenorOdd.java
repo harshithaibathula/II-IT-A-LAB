a9929a6d2bf60c32fda1c717c171f5a0a41e30fe
harshithaibathula <69143754+harshithaibathula@users.noreply.github.com>
2021-06-18 15:41:52 +0530

    Add files via upload

--- JAVA Lab/Lab Cycle -1/Check even or odd/EvenorOdd.java
+++ JAVA Lab/Lab Cycle -1/Check even or odd/EvenorOdd.java
@@ -1,1 +1,16 @@
-
+//check even or odd
+public class EvenorOdd
+{
+public static void main(String[] args)
+{
+int a = 8;
+if(a%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
+}
