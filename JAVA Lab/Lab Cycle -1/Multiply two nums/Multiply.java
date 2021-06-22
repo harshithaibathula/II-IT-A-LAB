d1805c0513b447939aa5f6237f72c4ecf09c14df
harshithaibathula <69143754+harshithaibathula@users.noreply.github.com>
2021-06-22 10:50:46 +0530

    Add files via upload

--- JAVA Lab/Lab Cycle -1/Multiply two nums/Multiply.java
+++ JAVA Lab/Lab Cycle -1/Multiply two nums/Multiply.java
@@ -1,1 +1,10 @@
-
+//multiply two numbers
+public class Multiply
+{
+public static void main(String[] args)
+{
+int a = 2,b = 3,product;
+product = a*b;
+System.out.println("Product of two numbers = "+ product);
+}
+}
