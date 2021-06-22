b5f6aa09114dfef64ece2bf0b61c343088d5d786
harshithaibathula <69143754+harshithaibathula@users.noreply.github.com>
2021-06-22 10:47:28 +0530

    Add files via upload

--- JAVA Lab/Lab Cycle -1/Area & crcumf of circle/Areaofcircle.java
+++ JAVA Lab/Lab Cycle -1/Area & crcumf of circle/Areaofcircle.java
@@ -1,1 +1,11 @@
-
+public class Areaofcircle
+{
+public static void main(String[] args)
+{
+int r=5;
+double area = 3.14*r*r;
+double circumf = 2*3.14*r;
+System.out.println("Area of circle = "+area);
+System.out.println("Circumference of circle = "+circumf);
+}
+}
