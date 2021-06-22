123126c9a1eac657a401514a510de34488fa9f45
harshithaibathula <69143754+harshithaibathula@users.noreply.github.com>
2021-06-22 11:12:52 +0530

    Add files via upload

--- JAVA Lab/Lab Cycle -1/Check vowel or constant/Checkvowelorconst.java
+++ JAVA Lab/Lab Cycle -1/Check vowel or constant/Checkvowelorconst.java
@@ -1,1 +1,22 @@
+public class Checkvowelorconst {
 
+    public static void main(String[] args) {
+
+        char ch = 'z';
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is vowel");
+                break;
+            default:
+                System.out.println(ch + " is consonant");
+        }
+    }
+}
+ 
+
+   
