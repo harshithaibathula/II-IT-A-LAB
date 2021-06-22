623ff1a3a21f21a2807be0975a83937f65645134
harshithaibathula <69143754+harshithaibathula@users.noreply.github.com>
2021-06-22 11:28:39 +0530

    Add files via upload

--- JAVA Lab/Lab Cycle -1/Calculate compound interest/Compinterest.java
+++ JAVA Lab/Lab Cycle -1/Calculate compound interest/Compinterest.java
@@ -1,1 +1,12 @@
-
+public class Compinterest
+{
+public static void main(String[] args)
+{
+int p = 2000;
+double r=0.08;
+int n =12;
+int t = 5;
+double compint = p*(1 + r/n)*(n*t)-p;
+System.out.println("Compound interest = "+compint);
+}
+}
