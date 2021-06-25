72b917493db27c83d8b24333dbe93972eee61d23
harshithaibathula <69143754+harshithaibathula@users.noreply.github.com>
2021-06-25 10:35:47 +0530

    Add files via upload

--- JAVA Lab/Lab Cycle -1/String in reverse order/Revstring.java
+++ JAVA Lab/Lab Cycle -1/String in reverse order/Revstring.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+class Revstring {
+   public static void main(String args[])
+   {
+      String reverseString="";
+      Scanner inpstr = new Scanner(System.in);
+      String inputString = inpstr.nextLine();
 
+      int length = inputString.length();
+
+      for ( int i = length - 1 ; i >= 0 ; i-- )
+         reverseString = reverseString + inputString.charAt(i);
+      
+      System.out.println("Given string in reverse order:- "+reverseString);
+
+   }
+}
