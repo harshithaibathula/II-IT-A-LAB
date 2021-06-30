eebdaa6b705a54ed0a4dfb44bbd1261b91aa9e28
harshithaibathula <69143754+harshithaibathula@users.noreply.github.com>
2021-06-30 18:57:45 +0530

    Add files via upload

--- JAVA Lab/Lab Cycle -1/Simple Calculator/Calculator.java
+++ JAVA Lab/Lab Cycle -1/Simple Calculator/Calculator.java
@@ -1,1 +1,36 @@
-
+import java.util.Scanner;
+public class Calculator
+{
+   public static void main(String[] args)
+   {
+      char operator;
+      float result;
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the values of a and b: ");
+      float a = sc.nextFloat();
+      float b = sc.nextFloat();
+      System.out.print("choose an opertor: ");
+      operator = sc.next().charAt(0);
+      switch(operator){
+       case '+':
+          result = a+b;
+          System.out.print("Sum of a and b is :" + result);
+          break;
+       case '-':
+          result = a-b;
+          System.out.print("Difference of a and b is :" + result);
+          break;
+       case '*':
+          result = a*b;
+          System.out.print("Multiplication of a and b is :" + result);
+          break;
+       case '/':
+          result = a/b;
+          System.out.print("Division of a and b is :" + result);
+          break;
+        default:
+        System.out.println("Invalid operator!");
+        break;
+      }
+   }
+}
