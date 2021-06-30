4862b0ebb1ccccdbfc42f1618bc8dc8484e312f7
harshithaibathula <69143754+harshithaibathula@users.noreply.github.com>
2021-06-30 18:38:09 +0530

    Add files via upload

--- JAVA Lab/Lab Cycle -1/Sorting array in ascending order/Sortarray.java
+++ JAVA Lab/Lab Cycle -1/Sorting array in ascending order/Sortarray.java
@@ -1,1 +1,35 @@
-
+import java.util.Scanner;
+public class Sortarray 
+{
+    public static void main(String[] args) 
+    {
+        int n, temp;
+        Scanner s = new Scanner(System.in);
+        System.out.print("No of elements in array:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for (int i = 0; i < n; i++) 
+        {
+            a[i] = s.nextInt();
+        }
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
+            {
+                if (a[i] > a[j]) 
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        System.out.print("Sorted Order:");
+        for (int i = 0; i < n - 1; i++) 
+        {
+            System.out.print(a[i] + ",");
+        }
+        System.out.print(a[n - 1]);
+    }
+}
