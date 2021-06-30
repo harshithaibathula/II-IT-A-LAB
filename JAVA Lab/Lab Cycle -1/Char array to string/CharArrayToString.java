c3b76831d01684b49d7671fe3a4c379f83da5d09
harshithaibathula <69143754+harshithaibathula@users.noreply.github.com>
2021-06-30 18:45:16 +0530

    Add files via upload

--- JAVA Lab/Lab Cycle -1/Char array to string/CharArrayToString.java
+++ JAVA Lab/Lab Cycle -1/Char array to string/CharArrayToString.java
@@ -1,1 +1,10 @@
-
+import java.util.Arrays;
+public class CharArrayToString
+{
+   public static void main(String args[])
+   {
+      char[] arr = {'H','e','l','l','o',' ','W','o','r','l','d'};
+      String s = new String(arr);
+      System.out.println(s);
+   }
+}
